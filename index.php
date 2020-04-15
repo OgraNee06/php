a3910609077cbf4fa7deb225f5ca8b84cf07748b
OgraNee06 <ogranee06@o2.pl>
2020-04-15 21:00:28 +0200

    basic changes

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-<?php
-    echo "Witaj " . htmlspecialchars($_GET["name"]) . "!";
-?>
+<html lang="pl">
+<body>
+
+<form method="post" action="index2.php"">
+Imię: <input type="text" name="fname">
+<input type="submit">
+</form>
+
+
+</body>
+</html>
