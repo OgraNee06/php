c5cac3cbcd9f87b8ed42b9742041ae617cdb6246
OgraNee06 <ogranee06@o2.pl>
2020-04-23 10:37:17 +0200

    zmiany

--- index2.php
+++ index2.php
@@ -1,12 +1,9 @@
 <?php
-$acceptName = 'Krystyna';
 $name = $_REQUEST['name'];
-($_SERVER["REQUEST_METHOD"] == "POST");
-if ($name == 'Krystyna')
+if ($name === 'Krystyna')
 {
     echo 'Welcome Krystyna';
 } else
 {
     echo "Żegnam " . $_POST["name"] . " Nie lubię cię! :C";
 }
-?>
