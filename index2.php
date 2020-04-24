b226b3f148b3780891b1105ef4c9773906a742d5
OgraNee06 <ogranee06@o2.pl>
2020-04-24 18:11:11 +0200

    nowe

--- index2.php
+++ index2.php
@@ -1,9 +1,27 @@
 <?php
 $name = $_REQUEST['name'];
-if ($name === 'Krystyna')
+$imiona= array(
+    'Krystyna',
+    'Andrzej'
+);
+if ($name === $imiona[1] )
 {
-    echo 'Welcome Krystyna';
-} else
-{
-    echo "Żegnam " . $_POST["name"] . " Nie lubię cię! :C";
+    echo 'Witaj ', $imiona[1];
 }
+if ($name === $imiona[0])
+{
+    echo 'Witaj ' , $imiona[0];
+}
+for($name1 = 'Krzysztof'; $name2 = 'Sandra';) {
+    switch ($name) {
+        case $name1;
+            echo 'Bycmoze Witam ', $name1;
+
+    }
+}
+
+
+
+
+
+
